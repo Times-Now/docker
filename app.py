97873574c72b6b66cdd249275d2a0ba68650a635
Suneha27 <57475942+Suneha27@users.noreply.github.com>
2019-12-14 18:17:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,96 @@
-from flask import Flask
-from redis import Redis, RedisError
+#!/usr/bin/env python
+from __future__ import print_function
+
+import codecs
 import os
-import socket
 
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+from setuptools import find_packages
+from setuptools import setup
 
-app = Flask(__name__)
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
 
-@app.route("/")
-def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+requirements = [
+    'six >= 1.4.0',
+    'websocket-client >= 0.32.0',
+    'requests >= 2.14.2, != 2.18.0',
+]
 
-    html = "<h3>Hello {name} team welcome to Bangalore World..!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "Itrainpulsar"), hostname=socket.gethostname(), visits=visits)
+extras_require = {
+    ':python_version < "3.5"': 'backports.ssl_match_hostname >= 3.5',
+    # While not imported explicitly, the ipaddress module is required for
+    # ssl_match_hostname to verify hosts match with certificates via
+    # ServerAltname: https://pypi.python.org/pypi/backports.ssl_match_hostname
+    ':python_version < "3.3"': 'ipaddress >= 1.0.16',
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081)
+    # win32 APIs if on Windows (required for npipe support)
+    # Python 3.6 is only compatible with v220 ; Python < 3.5 is not supported
+    # on v220 ; ALL versions are broken for v222 (as of 2018-01-26)
+    ':sys_platform == "win32" and python_version < "3.6"': 'pypiwin32==219',
+    ':sys_platform == "win32" and python_version >= "3.6"': 'pypiwin32==223',
+
+    # If using docker-py over TLS, highly recommend this option is
+    # pip-installed or pinned.
+
+    # TODO: if pip installing both "requests" and "requests[security]", the
+    # extra package from the "security" option are not installed (see
+    # https://github.com/pypa/pip/issues/4391).  Once that's fixed, instead of
+    # installing the extra dependencies, install the following instead:
+    # 'requests[security] >= 2.5.2, != 2.11.0, != 2.12.2'
+    'tls': ['pyOpenSSL>=17.5.0', 'cryptography>=1.3.4', 'idna>=2.0.0'],
+
+    # Only required when connecting using the ssh:// protocol
+    'ssh': ['paramiko>=2.4.2'],
+
+}
+
+version = None
+exec(open('docker/version.py').read())
+
+with open('./test-requirements.txt') as test_reqs_txt:
+    test_requirements = [line for line in test_reqs_txt]
+
+
+long_description = ''
+with codecs.open('./README.md', encoding='utf-8') as readme_md:
+    long_description = readme_md.read()
+
+setup(
+    name="docker",
+    version=version,
+    description="A Python library for the Docker Engine API.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/docker/docker-py',
+    project_urls={
+        'Documentation': 'https://docker-py.readthedocs.io',
+        'Changelog': 'https://docker-py.readthedocs.io/en/stable/change-log.html',  # noqa: E501
+        'Source': 'https://github.com/docker/docker-py',
+        'Tracker': 'https://github.com/docker/docker-py/issues',
+    },
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    install_requires=requirements,
+    tests_require=test_requirements,
+    extras_require=extras_require,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    maintainer='Joffrey F',
+    maintainer_email='joffrey@docker.com',
+)
